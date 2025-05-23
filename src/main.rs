03065865885073e24318c8e8e65a8314cc7a6e1c
Christopher Atherton <the8lack8ox@pm.me>
2025-05-23 00:46:40 -0400

    Fix jpeg typo

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ impl fmt::Display for ImageFormat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::Avif => write!(f, "avif"),
-            Self::Jpeg => write!(f, "jpg"),
+            Self::Jpeg => write!(f, "jpeg"),
             Self::Png => write!(f, "png"),
             Self::Webp => write!(f, "webp"),
         }
