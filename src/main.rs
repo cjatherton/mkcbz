f355a847698e5fdeb54186672cea73df8a1c8884
Christopher Atherton <cjatherton@proton.me>
2025-06-29 02:44:47 -0400

    Update email address

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //
-// Copyright 2024-2025 Christopher Atherton <the8lack8ox@pm.me>
+// Copyright 2024-2025 Christopher Atherton <cjatherton@proton.me>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the “Software”), to
